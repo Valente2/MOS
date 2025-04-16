6ffcd93a04a9421dc86b901b74c7660cf8455c5c
Valente <valentevescio@gmail.com>
2025-04-16 08:17:28 -0400

    adding new command: calc

---
+++ root/bin/calc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h> 
+int check(int x){
+  if(x == int
+
+int main(int argc, char *argv[])
+{
+  switch(argc)
+  {
+     case 4;
+     int num[] = {argv[2], argv[4]};
+     char opr = argv[3];
+     break;
+  }
+
+  switch(opr){
+     case "+";
+     printf("%d"
+    
+       
+     
+}
